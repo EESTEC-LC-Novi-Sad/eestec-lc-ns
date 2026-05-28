d1d30142d1c11bf4393d0d2cf0e1ee9b6b668222
sara-stojkov <sarastojkov6@gmail.com>
2026-05-28 15:12:20 +0200

    feat: separate different types of partners

--- src/app/components/pages/partners/partners.component.ts
+++ src/app/components/pages/partners/partners.component.ts
@@ -9,12 +9,30 @@ import { PartnerService } from 'src/app/services/partner.service';
 })
 export class PartnersComponent implements OnInit {
   public partners: Partner[] = [];
+  public merchPartners: Partner[] = [];
+  public mediaPartners: Partner[] = [];
+  public partnerOrganisations: Partner[] = [];
+
+  public loading = true;
 
   constructor(private partnerService: PartnerService) {}
 
   ngOnInit(): void {
     this.partnerService.getAllPartners().subscribe((data) => {
       this.partners = this.partnerService.convertDataToPartners(data);
+      this.loading = false;
+    });
+
+    this.partnerService.getAllPartnersByType('eestecMerchPartner').subscribe((data) => {
+      this.merchPartners = this.partnerService.convertDataToPartners(data);
+    });
+
+    this.partnerService.getAllPartnersByType('eestecMediaPartner').subscribe((data) => {
+      this.mediaPartners = this.partnerService.convertDataToPartners(data);
+    });
+
+    this.partnerService.getAllPartnersByType('eestecPartnerOrganisation').subscribe((data) => {
+      this.partnerOrganisations = this.partnerService.convertDataToPartners(data);
     });
   }
 }
